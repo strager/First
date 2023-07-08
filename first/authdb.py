0bf007f927ad7ce9f1cd0dd62ab76872536b9b69
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2023-07-07 22:32:06 -0300

    Add test for created_at time

--- first/authdb.py
+++ first/authdb.py
@@ -1,8 +1,10 @@
 """AuthDb"""
 import sqlite3
+from datetime import datetime
 
 UserId = int
 Token = str
+Time = datetime
 
 class AuthDb:
     def __init__(self):
@@ -28,3 +30,12 @@ class AuthDb:
         access_token, = result.fetchone()
         return access_token
 
+    def get_created_at_time(self, user_id: UserId) -> Time:
+        cur = self.db.cursor()
+        data = {
+            "user_id": user_id,
+        }
+        result = cur.execute("SELECT created_at FROM twitch_tokens WHERE user_id = :user_id", data)
+        created_at, = result.fetchone()
+        created_at = datetime.fromisoformat(created_at + "Z")
+        return created_at
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timezone
+import time
 import unittest
 from first.authdb import AuthDb
+
+TIMESTAMP_RESOLUTION = 1
 
 class TestAuth(unittest.TestCase):
 
@@ -11,6 +15,25 @@ class TestAuth(unittest.TestCase):
                 refresh_token="funnyrefreshtokenhere"
         )
         self.assertEqual("funnytokenhere", self.authdb.get_access_token(user_id=5))
+
+    def test_created_at_time_slow(self):
+        self.authdb = AuthDb()
+        before_add_user_time = datetime.now(timezone.utc)
+        time.sleep(TIMESTAMP_RESOLUTION)
+        self.authdb.add_new_user(
+                user_id=5,
+                access_token="funnytokenhere",
+                refresh_token="funnyrefreshtokenhere"
+        )
+        created_user_time = self.authdb.get_created_at_time(user_id=5)
+        time.sleep(TIMESTAMP_RESOLUTION)
+        after_created_user_time = datetime.now(timezone.utc)
+        print(before_add_user_time)
+        print(created_user_time)
+        print(after_created_user_time)
+        self.assertGreater(created_user_time, before_add_user_time)
+        self.assertGreater(after_created_user_time, created_user_time)
+
 
 if __name__ == '__main__':
     unittest.main()
